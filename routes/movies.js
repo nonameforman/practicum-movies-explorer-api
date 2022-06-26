0241d778f4b0504969ad5625942e7b0c2d8cb363
A Gordeev <nonameforman1@yandex.ru>
2022-06-26 14:50:54 +0900

    fix: celebrate post /movies - validate links

--- routes/movies.js
+++ routes/movies.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const { celebrate, Joi } = require('celebrate');
+const validator = require('validator');
 
 const {
   getFavMovies,
   saveMovie,
   deleteMovie,
 } = require('../controllers/movies');
+
+const validateURL = (value) => {
+  if (!validator.isURL(value, { require_protocol: true })) { throw new Error('Неправильный формат ссылки'); }
+  return value;
+};
 
 const moviesRouter = express.Router();
 
@@ -14,13 +20,13 @@ moviesRouter.post('/movies', celebrate({
   body: Joi.object().keys({
     country: Joi.string().required(),
     director: Joi.string().required(),
-    duration: Joi.number().required().positive(),
-    year: Joi.string().required(),
+    duration: Joi.number().required().positive().integer(),
+    year: Joi.string().required().pattern(/\d{4}/),
     description: Joi.string().required(),
-    image: Joi.string().required(),
-    trailerLink: Joi.string().required(),
-    thumbnail: Joi.string().required(),
-    movieId: Joi.number().required(),
+    image: Joi.string().required().custom(validateURL),
+    trailerLink: Joi.string().required().custom(validateURL),
+    thumbnail: Joi.string().required().custom(validateURL),
+    movieId: Joi.number().required().positive().integer(),
     nameRU: Joi.string().required(),
     nameEN: Joi.string().required(),
   }),
